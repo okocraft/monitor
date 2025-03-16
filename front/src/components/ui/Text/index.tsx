38420cd1b778e4667b5963a2f32942e6e2fdb1fe
Siroshun09 <siro.shun8@gmail.com>
2025-03-17 04:18:27 +0900

    feat(front): add hoverColor to Text component

--- front/src/components/ui/Text/index.tsx
+++ front/src/components/ui/Text/index.tsx
@@ -4,15 +4,18 @@ import styles from "./text.module.css";
 export type Props = {
     type?: keyof typeof types;
     color?: keyof typeof colors;
+    hoverColor?: keyof typeof hoverColors;
     className?: string;
     children: ReactNode;
 };
 
 export const Text = (props: Props) => {
+    const type = props.type ? types[props.type] : types.base;
+    const color = props.color ? colors[props.color] : colors.base;
+    const hoverColor = props.hoverColor ? hoverColors[props.hoverColor] : "";
+    const className = props.className ? props.className : "";
     return (
-        <span
-            className={`${props.type ? types[props.type] : types.base} ${props.color ? colors[props.color] : colors.base} ${props.className}`}
-        >
+        <span className={`${type} ${color} ${hoverColor} ${className}`}>
             {props.children}
         </span>
     );
@@ -27,4 +30,9 @@ const types = {
 const colors = {
     base: styles.baseColor,
     red: styles.redColor,
+    primary: styles.primaryColor,
+} as const;
+
+const hoverColors = {
+    primaryDark: styles.primaryDarkHoverColor,
 } as const;
