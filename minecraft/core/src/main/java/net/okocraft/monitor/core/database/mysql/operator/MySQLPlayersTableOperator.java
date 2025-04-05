ac63740a8bd3b4b14915d32a3590b4f588f55e07
Siroshun09 <siro.shun8@gmail.com>
2025-04-06 00:04:34 +0900

    fix(minecraft): pass uuid bytes instead of using UUID_TO_BIN

--- minecraft/core/src/main/java/net/okocraft/monitor/core/database/mysql/operator/MySQLPlayersTableOperator.java
+++ minecraft/core/src/main/java/net/okocraft/monitor/core/database/mysql/operator/MySQLPlayersTableOperator.java
@@ -9,6 +9,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.UUID;
 
 @NotNullByDefault
@@ -16,8 +17,8 @@ public class MySQLPlayersTableOperator implements PlayersTableOperator {
 
     @Override
     public @Nullable MonitorPlayer getPlayerByUUID(Connection connection, UUID uuid) throws SQLException {
-        try (PreparedStatement statement = connection.prepareStatement("SELECT id, name FROM minecraft_players WHERE uuid = UUID_TO_BIN(?)")) {
-            statement.setString(1, uuid.toString());
+        try (PreparedStatement statement = connection.prepareStatement("SELECT id, name FROM minecraft_players WHERE uuid = ?")) {
+            statement.setBytes(1, MySQLUUID.uuidToBytes(uuid));
             try (ResultSet resultSet = statement.executeQuery()) {
                 if (resultSet.next()) {
                     return new MonitorPlayer(
@@ -33,8 +34,8 @@ public class MySQLPlayersTableOperator implements PlayersTableOperator {
 
     @Override
     public int insertPlayer(Connection connection, UUID uuid, String name) throws SQLException {
-        try (PreparedStatement statement = connection.prepareStatement("INSERT INTO minecraft_players (uuid, name) VALUES (UUID_TO_BIN(?), ?)")) {
-            statement.setString(1, uuid.toString());
+        try (PreparedStatement statement = connection.prepareStatement("INSERT INTO minecraft_players (uuid, name) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS)) {
+            statement.setBytes(1, MySQLUUID.uuidToBytes(uuid));
             statement.setString(2, name);
             statement.executeUpdate();
             try (ResultSet resultSet = statement.getGeneratedKeys()) {
---
+++ minecraft/core/src/main/java/net/okocraft/monitor/core/database/mysql/operator/MySQLUUID.java
@@ -0,0 +1,26 @@
+package net.okocraft.monitor.core.database.mysql.operator;
+
+import org.jetbrains.annotations.NotNullByDefault;
+
+import java.nio.ByteBuffer;
+import java.util.UUID;
+
+@NotNullByDefault
+final class MySQLUUID {
+
+    static byte[] uuidToBytes(UUID uuid) {
+            ByteBuffer bb = ByteBuffer.wrap(new byte[16]);
+            bb.putLong(uuid.getMostSignificantBits());
+            bb.putLong(uuid.getLeastSignificantBits());
+            return bb.array();
+    }
+
+    static UUID bytesToUUID(byte[] bytes) {
+        ByteBuffer bb = ByteBuffer.wrap(bytes);
+        return new UUID(bb.getLong(), bb.getLong());
+    }
+
+    private MySQLUUID() {
+        throw new UnsupportedOperationException();
+    }
+}
