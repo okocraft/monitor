47a2a328e92809b7b979b50be4306e58e919add9
Siroshun09 <siro.shun8@gmail.com>
2025-04-05 22:54:09 +0900

    build(minecraft): add ConcurrentUtil

--- minecraft/core/build.gradle.kts
+++ minecraft/core/build.gradle.kts
@@ -2,7 +2,18 @@ plugins {
     id("monitor.common-conventions")
 }
 
+repositories {
+    maven {
+        name = "paper"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+        content {
+            includeGroup("ca.spottedleaf")
+        }
+    }
+}
+
 dependencies {
     implementation(libs.hikaricp)
     implementation(libs.configapi.format.yaml)
+    compileOnly(libs.concurrent.util)
 }
