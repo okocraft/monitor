9f9916daa27309670ea40489be92f41b5aea0798
Siroshun09 <siro.shun8@gmail.com>
2025-06-08 13:36:59 +0900

    fix(minecraft/core): create parent directories on uploading to local

--- minecraft/core/src/main/java/net/okocraft/monitor/core/cloud/storage/local/LocalStorage.java
+++ minecraft/core/src/main/java/net/okocraft/monitor/core/cloud/storage/local/LocalStorage.java
@@ -1,6 +1,7 @@
 package net.okocraft.monitor.core.cloud.storage.local;
 
 import dev.siroshun.codec4j.api.encoder.Encoder;
+import dev.siroshun.codec4j.api.file.DefaultOpenOptions;
 import dev.siroshun.codec4j.io.gson.GsonIO;
 import dev.siroshun.jfun.result.Result;
 import net.okocraft.monitor.core.cloud.storage.CloudStorage;
@@ -41,7 +42,18 @@ public class LocalStorage implements CloudStorage {
 
     @Override
     public <T> Result<Void, UploadError> upload(String key, Encoder<T> encoder, T object) {
-        try (OutputStream out = Files.newOutputStream(this.directory.resolve(key + FILE_EXTENSION));
+        Path filepath = this.directory.resolve(key + FILE_EXTENSION);
+
+        Path parent = filepath.getParent();
+        if (parent != null && !Files.isDirectory(parent)) {
+            try {
+                Files.createDirectories(parent);
+            } catch (IOException e) {
+                return Result.failure(new UploadError.UploadException(e));
+            }
+        }
+
+        try (OutputStream out = Files.newOutputStream(filepath, DefaultOpenOptions.fileOpenOptions());
              BufferedOutputStream bufferedOut = new BufferedOutputStream(out, BUFFER_SIZE);
              GZIPOutputStream gzipOut = new GZIPOutputStream(bufferedOut) {
                  {
