63b93f19ffb2c723ed2c7ede09e478b3b33a4ea5
Siroshun09 <siro.shun8@gmail.com>
2025-07-17 10:03:48 +0900

    build(app): update oapi-codegen to v2.5.0, and use tool directive

--- app/go.mod
+++ app/go.mod
@@ -44,6 +44,7 @@ require (
 	github.com/onsi/gomega v1.29.0 // indirect
 	github.com/perimeterx/marshmallow v1.1.5 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/speakeasy-api/jsonpath v0.6.0 // indirect
 	github.com/speakeasy-api/openapi-overlay v0.10.2 // indirect
 	github.com/vmware-labs/yaml-jsonpath v0.3.2 // indirect
 	golang.org/x/mod v0.22.0 // indirect
@@ -54,3 +55,5 @@ require (
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
+
+tool github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen
--- app/tools/oapi-codegen/tools.go
+++ app/tools/oapi-codegen/tools.go
@@ -1,11 +1,4 @@
-//go:build tools
-// +build tools
+package oapi_codegen
 
-package main
-
-import (
-	_ "github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen"
-)
-
-//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=api.yml ../../../schema/openapi/monitor-api.yml
-//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=embeded-spec.yml ../../../schema/openapi/monitor-api.yml
+//go:generate go tool oapi-codegen --config=api.yml ../../../schema/openapi/monitor-api.yml
+//go:generate go tool oapi-codegen --config=embeded-spec.yml ../../../schema/openapi/monitor-api.yml
